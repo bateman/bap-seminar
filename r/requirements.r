e3332177773d55d4b2339cd1982101d9a3e5608c
Fabio Calefato <fcalefato@gmail.com>
2016-10-08 12:30:12 +0100

    added more packages

--- r/requirements.r
+++ r/requirements.r
@@ -22,3 +22,27 @@ if(!require("ScottKnott", quietly = TRUE)){
 if(!require("caret", quietly = TRUE)){
   install.packages(c("caret", "car", "pbkrtest", "lme4"), repos = "http://cran.mirror.garr.it/mirrors/CRAN/")
 }
+
+# Boruta package for wrapper-based feature sealection
+if(!require("Boruta", quietly = TRUE)){
+  install.packages(c("Boruta"), repos = "http://cran.mirror.garr.it/mirrors/CRAN/")
+}
+
+# Naive Bayes
+if(!require("klaR", quietly = TRUE)){
+  install.packages(c("klaR"), repos = "http://cran.mirror.garr.it/mirrors/CRAN/")
+}
+
+if(!require("e1071", quietly = TRUE)){
+  install.packages(c("e1071"), repos = "http://cran.mirror.garr.it/mirrors/CRAN/")
+}
+
+# C5.0 decision trees library
+if(!require("C50", quietly = TRUE)){
+  install.packages(c("C50"), repos = "http://cran.mirror.garr.it/mirrors/CRAN/")
+}
+
+# Random Forest
+if(!require("randomForest", quietly = TRUE)){
+  install.packages(c("randomForest"), repos = "http://cran.mirror.garr.it/mirrors/CRAN/")
+}
